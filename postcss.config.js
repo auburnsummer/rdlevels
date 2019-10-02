54ca0125f4e1b11a1839b501c78f893e7d0f3ff1
ladybug <auburnete@gmail.com>
2019-10-02 11:34:18 +1000

    purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,13 @@ module.exports = {
     plugins: [
       require('postcss-import'),
       require('tailwindcss'),
-      require('autoprefixer')
+      require('autoprefixer'),
+      require('@fullhuman/postcss-purgecss')({
+        content: [
+          './*.html',
+          './pagination.js'
+        ],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      })
     ]
   }
