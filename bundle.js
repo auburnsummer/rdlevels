e27d9e2627abadc8b383f8a2557f7d50ab24b992
Andrew Li <flameyanai@gmail.com>
2021-02-10 11:53:13 -0500

    Fix number of pages

--- bundle.js
+++ bundle.js
@@ -122,15 +122,19 @@ var app = new Vue({
       params: {}
     }, 
     computed: {
-      truncated: function() {
-        let sorted = this.sorted(this.search_results);
+      filtered: function() {
+        let levels = this.search_results;
         if(!this.showUnverifiedLevels && !this.searchQuery.includes('booster')) {
-          sorted = sorted.filter((doc) => doc.verified);
+          levels = levels.filter((doc) => doc.verified);
         }
+        return levels;
+      },
+      truncated: function() {
+        const sorted = this.sorted(this.filtered);
         return _.chunk(sorted, this.limit)[this.currentPage];
       },
       numberOfPages: function() {
-        return _.chunk(this.search_results, this.limit).length;
+        return _.chunk(this.filtered, this.limit).length;
       },
       currentPage: {
         get: function() {
